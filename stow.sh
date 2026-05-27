50654d0b78f08a30e7f86087eb627eb0f2e7af13
hamza <hamza@example.com>
2026-05-27 22:25:27 +0500

    dotfile mirroring system (renaming one folder created this massive commit!)

--- stow.sh
+++ stow.sh
@@ -1,7 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-shopt -s extglob
-for dir in */; do
-    [[ "$dir" != .git*"/" ]] && stow -d "$(pwd)" "$dir" && echo Stowed $dir
-done
-echo Stow complete
+HOSTNAME=$(cat /proc/sys/kernel/hostname)
+RESET="\033[0m"
+GREEN="\033[32m"
+BLUE="\033[34m"
+YELLOW="\033[33m"
+
+echo -e "Targeting '${HOSTNAME}'\n"
+
+echo -e "    ${GREEN}Stowing 'base' profile...${BLUE}"
+stow -v -t ~ base 2>&1 | sed "s/^/       /"
+echo -e "${RESET}"
+
+if [ -d "$HOSTNAME" ]; then
+    echo -e "    ${GREEN}Stowing [${HOSTNAME}]...${BLUE}"
+    stow -v -t ~ "$HOSTNAME" 2>&1 | sed "s/^/          /"
+else
+    echo -e "    ${YELLOW}[!] Profile '${HOSTNAME}' not found.${RESET}"
+    echo -e "    ${GREEN}Stowing 'fallback'...${BLUE}"
+    stow -v -t ~ fallback 2>&1 | sed "s/^/       /"
+fi
+
+echo -e "\n${RESET}Complete!"
