3a0e3ff2b76689cacaa6588caccca36b67a66d91
hamza <hamza@example.com>
2026-05-28 19:57:00 +0500

    luastatus tuned module

---
+++ base/.config/luastatus/tuned.lua
@@ -0,0 +1,31 @@
+local profiles = {
+	["powersave"] = "balanced-battery";
+	["balanced-battery"] = "balanced";
+	["balanced"] = "throughput-performance";
+	["throughput-performance"] = "powersave";
+}
+
+widget = {
+	plugin = "timer",
+	opts = {interval = 2};
+	cb = function()
+		local profile
+		local file = io.open("/etc/tuned/active_profile", "r")
+		if file then
+			local content = file:read("*a")
+			file:close()
+			profile = content:gsub("%s+", "")
+		else
+			profile = "balanced"
+		end
+
+		return {full_text = "[PWR: " .. profile .. "]"; instance = profile; color = '#ebbcba'; seperator = false; seperator_block_width = 12}
+	end;
+	event = function(ctx)
+		if ctx.button == 1 then
+			local current_profile = ctx.instance or "balanced"
+			local next_profile = profiles[current_profile] or "balanced"
+			os.execute("tuned-adm -a profile " .. next_profile)
+		end
+	end;
+}
